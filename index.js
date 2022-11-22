ea8e7303beaf47173f3d4f460a3405b9aea3939e
Lucas Branco <luqholanda@gmail.com>
2022-11-22 17:33:16 -0300

    create readme.md

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const base64 = require('base64topdf');
 const fs = require('fs');
 
@@ -5,6 +6,7 @@ if (process.argv.length <= 2) {
     console.log(new Error("Usage example:\n\tconvert-base64 base64-file-name.txt\n\tconvert-base64 base64-file-name.txt converted.pdf\n\tconvert-base64 JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3RoIDg0OC9UeXBlL09ialN0bT4...\n\tconvert-base64 JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3RoIDg0OC9UeXBlL09ialN0bT4... converted.pdf"));
 }
 
+var folder = process.cwd();
 var thirdCommand = process.argv[2];
 var fourthCommand = process.argv.length > 3 ? process.argv[3] : "converted.pdf";
 var base64String = false;
@@ -15,11 +17,10 @@ if (thirdCommand.indexOf("JVBER") > -1) {
     data = thirdCommand;
 }
 
-console.log(thirdCommand);
-console.log(fourthCommand);
-
 if (!base64String) {
-    data = fs.readFileSync(thirdCommand, 'utf8');
+    data = fs.readFileSync(`${folder}/${thirdCommand}`, 'utf8');
 }
 
-base64.base64Decode(data, fourthCommand);
+base64.base64Decode(data, `${folder}/${fourthCommand}`);
+
+console.log(`Base64 was successfully converted with name ${fourthCommand}`);
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Convert Base64 to PDF
+
+## Installation
+Just run the command bellow
+
+```console
+npm install -g node-terminal-base64-2-pdf-converter
+```
+
+## Usage
+```console
+convert-base64-pdf base64.txt
+```
+
+OR
+
+```console
+convert-base64-pdf base64.txt converted-filename.pdf
+```
+
+OR
+
+```console
+convert-base64-pdf YOUR_BASE64_STRING
+```
+
+OR
+
+```console
+convert-base64-pdf YOUR_BASE64_STRING converted-filename
+```
+
+All converted files will be placed in the same folder that you ran command. On this first version you can't put complete URI from file, you will need to navigate with 'cd' command to file location or using string base64 instead.
